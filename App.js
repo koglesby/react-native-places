2fa6d9c85b1aabdc3682bd41f1c38a545c003e86
Kevin Oglesby <kevinjoglesby@gmail.com>
2018-04-05 13:07:21 -0700

    separated PlaceInput and PlaceList into own components

--- App.js
+++ App.js
@@ -2,47 +2,27 @@ import React, { Component } from 'react';
 import { StyleSheet, Text, View , TextInput, Button } from 'react-native';
 
 import ListItem from './src/components/ListItem/ListItem';
+import PlaceInput from './src/components/PlaceInput/PlaceInput';
+import PlaceList from './src/components/PlaceList/PlaceList';
 
 export default class App extends Component {
   state = {
-    placeName: '',
     places: []
   };
 
-  placeNameChangedHandler = (val) => {
-    this.setState({
-      placeName: val
-    })
-  };
-
-  placeSubmitHandler = () => {
-    if (this.state.placeName.trim() === "") {
-      return;
-    }
-
+  placeAddedHandler = placeName => {
     this.setState(prevState => {
       return {
-        places: prevState.places.concat(prevState.placeName)
+        places: prevState.places.concat(placeName)
       };
     })
   };
 
   render() {
-    const placesOutput = this.state.places.map((place, i) => (
-      <ListItem key={i} placeName={place}/>
-    ));
     return (
       <View style={styles.container}>
-        <View style={styles.inputContainer}>
-          <TextInput
-            style={styles.placeInput}
-            placeholder="An awesome place"
-            value={this.state.placeName}
-            onChangeText={this.placeNameChangedHandler}
-          />
-          <Button onPress={this.placeSubmitHandler} title="Add" style={styles.placeButton}/>
-        </View>
-        <View style={styles.listContainer}>{placesOutput}</View>
+        <PlaceInput onPlaceAdded={this.placeAddedHandler} />
+        <PlaceList places={this.state.places}/>
       </View>
     );
   }
@@ -55,20 +35,5 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'flex-start',
-  },
-  inputContainer: {
-    width: "100%",
-    flexDirection: "row",
-    justifyContent: "space-between",
-    alignItems: "center"
-  },
-  placeInput: {
-    width: "75%"
-  },
-  placeButton: {
-    width: "25%"
-  },
-  listContainer: {
-    width: "100%"
   }
 });
---
+++ src/components/PlaceInput/PlaceInput.js
@@ -0,0 +1,57 @@
+import React, { Component } from 'react';
+import {View, TextInput, Button, StyleSheet} from 'react-native';
+
+class PlaceInput extends Component {
+  state = {
+    placeName: '',
+    places: []
+  };
+
+  placeNameChangedHandler = (val) => {
+    this.setState({
+      placeName: val
+    })
+  };
+
+  placeSubmitHandler = () => {
+    if (this.state.placeName.trim() === "") {
+      return;
+    }
+    this.props.onPlaceAdded(this.state.placeName);
+  };
+
+  render() {
+    return(
+      <View style={styles.inputContainer}>
+        <TextInput
+          style={styles.placeInput}
+          placeholder="An awesome place"
+          value={this.state.placeName}
+          onChangeText={this.placeNameChangedHandler}
+        />
+        <Button
+          onPress={this.placeSubmitHandler}
+          title="Add"
+          style={styles.placeButton}
+        />
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  inputContainer: {
+    width: "100%",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center"
+  },
+  placeInput: {
+    width: "75%"
+  },
+  placeButton: {
+    width: "25%"
+  },
+});
+
+export default PlaceInput;
