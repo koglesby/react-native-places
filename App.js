01be2056b7a30b8471ae80262a827cd075736082
Kevin Oglesby <kevinjoglesby@gmail.com>
2018-05-08 14:43:35 -0700

    added tabs screen

--- App.js
+++ App.js
@@ -1,12 +1,14 @@
 import { Navigation } from 'react-native-navigation';
 
 import AuthScreen from './src/screens/Auth/Auth';
-
+import SharePlaceScreen from "./src/screens/SharePlace/SharePlace";
+import FindPlaceScreen from './src/screens/FindPlace/FindPlace';
 
 //Register Screens
 
 Navigation.registerComponent("sweet-places.AuthScreen", () => AuthScreen);
-
+Navigation.registerComponent("sweet-places.SharePlaceScreen", () => SharePlaceScreen);
+Navigation.registerComponent("sweet-places.FindPlaceScreen", () => FindPlaceScreen);
 // Start App
 
 Navigation.startSingleScreenApp({
