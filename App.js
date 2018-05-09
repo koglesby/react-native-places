f7723b6f6bf7e78e043861556a16a7aeb4d66666
Kevin Oglesby <kevinjoglesby@gmail.com>
2018-05-08 17:05:00 -0700

    connected shareplace and findplace tabs to redux

--- App.js
+++ App.js
@@ -1,14 +1,32 @@
 import { Navigation } from 'react-native-navigation';
-
+import { Provider } from 'react-redux';
 import AuthScreen from './src/screens/Auth/Auth';
 import SharePlaceScreen from "./src/screens/SharePlace/SharePlace";
 import FindPlaceScreen from './src/screens/FindPlace/FindPlace';
+import configureStore from './src/store/configureStore';
+
+const store = configureStore();
 
 //Register Screens
 
-Navigation.registerComponent("sweet-places.AuthScreen", () => AuthScreen);
-Navigation.registerComponent("sweet-places.SharePlaceScreen", () => SharePlaceScreen);
-Navigation.registerComponent("sweet-places.FindPlaceScreen", () => FindPlaceScreen);
+Navigation.registerComponent(
+  "sweet-places.AuthScreen",
+  () => AuthScreen,
+  store,
+  Provider
+);
+Navigation.registerComponent(
+  "sweet-places.SharePlaceScreen",
+  () => SharePlaceScreen,
+  store,
+  Provider
+);
+Navigation.registerComponent(
+  "sweet-places.FindPlaceScreen",
+  () => FindPlaceScreen,
+  store,
+  Provider
+);
 // Start App
 
 Navigation.startSingleScreenApp({
