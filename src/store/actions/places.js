1b2d1d441735220bd68f13c781d977805e31b046
Kevin Oglesby <kevinjoglesby@gmail.com>
2018-06-06 20:31:00 -0700

    installed redux-thunk, post to firebase

--- src/store/actions/places.js
+++ src/store/actions/places.js
@@ -1,12 +1,27 @@
 import { ADD_PLACE, DELETE_PLACE } from './actionTypes';
 
 export const addPlace = (placeName, location, image) => {
-  return {
-    type: ADD_PLACE,
-    placeName: placeName,
-    location: location,
-    image: image
+  return dispatch => {
+    const placeData = {
+      name: placeName,
+      location: location
+    };
+    fetch("https://rn-places.firebaseio.com/places.json", {
+      method: "POST",
+      body: JSON.stringify(placeData)
+    })
+      .catch(err => console.log(err))
+      .then(res => res.json())
+      .then(parsedRes => {
+        console.log(parsedRes);
+      });
   };
+  // {
+  //   type: ADD_PLACE,
+  //   placeName: placeName,
+  //   location: location,
+  //   image: image
+  // };
 };
 
 export const deletePlace = (key) => {
